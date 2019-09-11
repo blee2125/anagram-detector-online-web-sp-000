4b7ec5825d58ee294da5ca2bbced76a9f8045f66
favorable-device-5158 <blee2125@gmail.com>
2019-09-11 13:27:12 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,12 @@
 class Anagram
   attr_accessor :some_word
 
-  def initialize(some_words)
+  def initialize(some_word)
     @some_word = some_word
   end
 
-  def match
-
+  def match(array)
+    array.select { |e| e.split("") == @some_word.split("") }
   end
 
 end
